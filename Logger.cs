3af4607d3d47e4750b7248cc1d2206e3c3446cbb
Sayali <Sayali@DESKTOP-E2SFRTN>
2024-12-12 10:04:43 -0800

    Add project files.

---
+++ Logger.cs
@@ -0,0 +1,12 @@
+﻿namespace ExceptionLogger
+{
+    public static class Logger
+    {
+        public static void Log(Exception ex)
+        {
+            string logPath = "log.txt"; // Log file path
+            string logMessage = $"[{DateTime.Now}] {ex.Message}\n{ex.StackTrace}\n";
+            File.AppendAllText(logPath, logMessage);
+        }
+    }
+}
